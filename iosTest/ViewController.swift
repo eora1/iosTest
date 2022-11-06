645850743e805f7811c15c9ce4e2474f4d6cecbc
Daniil <daniilgavriluk765@gmail.com>
2022-11-07 01:36:56 +0300

    Initial Commit

---
+++ iosTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosTest
+//
+//  Created by Даниил Гаврилюк on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
